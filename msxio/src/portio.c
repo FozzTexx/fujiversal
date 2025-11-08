fe77586c8ee2f161253d9256919a98c47861fcb7
Chris Osborn <fozztexx@fozztexx.com>
2025-11-08 09:15:06 -0800

    Add constants for i8255 modes

--- msxio/src/portio.c
+++ msxio/src/portio.c
@@ -12,12 +12,25 @@
 #define INBUF_FULL  0x20 // i8255 received the byte, IBF, output active high
 #define INBUF_GET   0x10 // ESP32 wants to send, /STB, input active low
 
+#define DATA_DIR    0x01
+#define DATA_EN     0x02
+#define ESP32_EN    0x04
+
+#define I8255_G2_PORTC_INPUT 0x01
+#define I8255_G2_PORTB_INPUT 0x02
+#define I8255_G2_MODE_1      0x04
+#define I8255_G1_PORTC_INPUT 0x08
+#define I8255_G1_PORTA_INPUT 0x10
+#define I8255_G1_MODE_1      0x20
+#define I8255_G1_MODE_2      0x40
+#define I8255_MODE_ACTIVE    0x80
+
 void port_init()
 {
-  z80_outp(PCTRL,0xC0);
-  z80_outp(PORTC,0x06);
-  z80_outp(PORTC,0x02);
-  z80_outp(PORTC,0x06);
+  z80_outp(PCTRL,I8255_MODE_ACTIVE | I8255_G1_MODE_2);
+  z80_outp(PORTC,DATA_EN | ESP32_EN);
+  z80_outp(PORTC,DATA_EN);
+  z80_outp(PORTC,DATA_EN | ESP32_EN);
   msleep(200);
   z80_inp(PORTA); // Flush input
 
@@ -40,12 +53,20 @@ int port_getc()
 
 int port_getc_timeout(uint16_t timeout)
 {
-  int b;
+    int b=-1;
+    uint16_t start;
 
-  // FIXME - actually timeout
-  while ((b = port_getc()) < 0)
-    ;
-  return b;
+
+    // Store start time so we can calculate delta even when TIKCNT overflows
+    start = wpeek(TIKCNT);
+
+    while ((wpeek(TIKCNT) - start) < timeout)
+    {
+        b = port_getc();
+        if (b > -1)
+            return b;
+    }
+    return b;
 }
 
 uint16_t port_getbuf(void *buf, uint16_t len, uint16_t timeout)
@@ -54,7 +75,6 @@ uint16_t port_getbuf(void *buf, uint16_t len, uint16_t timeout)
   int b;
   uint8_t *ptr = (uint8_t *) buf;
 
-`
   for (idx = 0; idx < len; idx++) {
     b = port_getc_timeout(timeout);
     if (b < 0)
