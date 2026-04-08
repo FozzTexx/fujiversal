41cf19b68ff429f9bbd48988ae9bff3e0009d5c8
Chris Osborn <fozztexx@fozztexx.com>
2026-04-08 12:56:02 -0700

    Remove obsolete port_* functions

--- msx/unapi/src/portio.h
+++ msx/unapi/src/portio.h
@@ -1,34 +1,22 @@
 #include <stdint.h>
 
-#if 0
 // returns signed int with data or -1 if no data is available
 extern int __FASTCALL__ port_getc();
 
 // return data if it arrives before timeout or -1 if timeout expires
 extern int __FASTCALL__ port_getc_timeout(uint16_t timeout);
 
-// reads data until c is seen or timeout occurs, returns c or -1 on timeout
-// timeout *does not reset* when a character is received
-extern int __CALLEE__ port_discard_until(uint8_t c, uint16_t timeout);
-
+// reads and decodes SLIP into two buffers
 // returns length of data received, if timeout expires returns all data received until then
 // timeout resets when a character is received
-extern uint16_t __CALLEE__ port_getbuf(void *buf, uint16_t len, uint16_t timeout);
-
-// reads until c is found, timeout, or maxlen is reached
-// returns length of data received, including c
-// timeout resets when a character is received
-extern uint16_t __CALLEE__ port_get_until(void *buf, uint16_t maxlen, uint8_t c,
-                                          uint16_t timeout);
-
-// writes data to port, returns number of bytes written
-extern uint16_t __CALLEE__ port_putbuf(void *buf, uint16_t len);
-#endif
-
 extern uint16_t __CALLEE__ port_getbuf_slip_dual(void *hdr_buf, uint16_t hdr_len,
                                                  void *data_buf, uint16_t data_len,
                                                  uint16_t timeout);
+
+// writes character to port
 extern void __FASTCALL__ port_putc(uint8_t c);
+
+// writes data to port handling SLIP escapes, returns number of bytes written
 extern uint16_t __CALLEE__ port_putbuf_slip(const void *buf, uint16_t len);
 
 #define VDP_IS_PAL (((unsigned char *) 0x002b) & 0x80)
